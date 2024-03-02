47c1a3cf78f29bf89c3ddd31733cd646476ec3bb
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-03-03 00:50:25 +0200

    auth

--- build_it/settings.py
+++ build_it/settings.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
