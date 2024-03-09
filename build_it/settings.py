be096018f2abab01cf95870cd4660644e913f56b
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-03-10 00:09:41 +0200

    build

--- build_it/settings.py
+++ build_it/settings.py
@@ -139,16 +139,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 # if not DEBUG:
 #     DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/build_it_server',
 #         conn_max_age=600)}
