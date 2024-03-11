9629007a1694b3598d8560c4938e7c21e8a4a7e6
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-03-12 00:04:46 +0200

    debug-false .env

--- build_it/settings.py
+++ build_it/settings.py
@@ -28,14 +28,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEBUG')
 
-ALLOWED_HOSTS = [f"{os.getenv("SERVER_BASE_URL")}",
-                 "http://localhost",
-                 "http://127.0.0.1",
-                 'http://localhost:3000',
-                 'localhost',
-                 '*']
+ALLOWED_HOSTS = [
+    # 'localhost',
+    # f"{os.getenv("SERVER_BASE_URL")}",
+    # "http://localhost",
+    # "http://127.0.0.1",
+    # 'http://localhost:3000',
+    '*'
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
