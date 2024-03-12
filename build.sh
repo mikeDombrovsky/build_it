b3911eec776fb36ef60f705344e96c01681a6ebf
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-03-12 22:22:44 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 #python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput --username admin --email admin@gmail.com --password $DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser --noinput --username admin --email admin@gmail.com
