82df7846150302785448c013e7568d1c339ad8af
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-03-25 23:38:02 +0200

    admin page css fix

--- build_it/settings.py
+++ build_it/settings.py
@@ -206,7 +206,7 @@ if os.getenv('RENDER'):
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
